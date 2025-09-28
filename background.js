f15cc066c25547b267f73943dbd2a0e47d71a10a
danisrrrr <155295983+danisrrrr@users.noreply.github.com>
2025-09-28 21:14:14 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.includes("docs.google.com/forms") && !tab.url.includes("edit")) {
+  if (tab.url.includes("https://vote.breaktudoawards.com/") && !tab.url.includes("edit")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ["content.js"]
